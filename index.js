0893c8aebef5e10317888814d5b45ef294d7516b
WingUNO <le13413580712@163.com>
2017-04-23 23:53:57 +0800

    dropdown

--- index.js
+++ index.js
@@ -1,114 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { Table } from 'antd';
+import { Menu, Dropdown, Button, Icon, message } from 'antd';
 
-const columns = [{
-  title: 'No',
-  dataIndex: 'no'
-},{
-  title: 'Name',
-  dataIndex: 'name',
-  filters: [{
-    text: 'Joe',
-    value: 'Joe',
-  }, {
-    text: 'Jim',
-    value: 'Jim',
-  }, {
-    text: 'Submenu',
-    value: 'Submenu',
-    children: [{
-      text: 'Green',
-      value: 'Green',
-    }, {
-      text: 'Black',
-      value: 'Black',
-    }],
-  }],
-  // specify the condition of filtering result
-  // here is that finding the name started with `value`
-  onFilter: (value, record) => record.name.indexOf(value) === 0,
-  sorter: (a, b) => a.name.length - b.name.length,
-}, {
-  title: 'Age',
-  dataIndex: 'age',
-  sorter: (a, b) => a.age-b.age,
-}, {
-  title: 'Address',
-  dataIndex: 'address',
-  filters: [{
-    text: 'London',
-    value: 'London',
-  }, {
-    text: 'New York',
-    value: 'New York',
-  }],
-  filterMultiple: false,
-  onFilter: (value, record) => record.address.indexOf(value) === 0,
-  sorter: (a, b) => a.address.length - b.address.length,
-}];
-
-const data = [{
-  key: '1',
-  no: 1,
-  name: 'John Brown',
-  age: 32,
-  address: 'New York No. 1 Lake Park',
-}, {
-  key: '2',
-  no: 2,
-  name: 'Jim Green',
-  age: 42,
-  address: 'London No. 1 Lake Park',
-}, {
-  key: '3',
-  no: 3,
-  name: 'Joe Black',
-  age: 32,
-  address: 'Sidney No. 1 Lake Park',
-}, {
-  key: '4',
-  no: 4,
-  name: 'Jim Red',
-  age: 32,
-  address: 'London No. 2 Lake Park',
-}];
-
-// function onChange(pagination, filters, sorter) {
-//   console.log('params', pagination, filters, sorter);
-// }
-
-class MyTable extends React.Component{
-  constructor(props) {
-    super(props);
-    this.state={
-      data:data,
-      columns:columns
-    }
-  }
-
-  onChange = (pagination, filters, sorter) => {
-    console.log(pagination, filters, sorter);
-    // let data = [...this.state.data];
-    // data.sort((a, b) => {
-    //   return sorter.order == 'ascend' ? a[sorter.columnKey] - b[sorter.columnKey] : b[sorter.columnKey] - a[sorter.columnKey];
-    // })
-    // data.forEach((value, index) => {
-    //   value.no = index + 1;
-    // })
-    // this.setState({
-    //   data
-    // })
-    // this.forceUpdate();
-  }
-
-  render() {
-    console.log('update.....');
-    return (
-      <Table columns={this.state.columns} dataSource={this.state.data} onChange={this.onChange} />
-    );
-  }
+function handleButtonClick(e) {
+  message.info('Click on left button.');
+  console.log('click left button', e);
 }
 
-ReactDOM.render(<MyTable/>, document.getElementById('root'));
+function handleMenuClick(e) {
+  message.info('Click on menu item.');
+  console.log('click', e);
+}
+
+const menu = (
+  <Menu onClick={handleMenuClick}>
+    <Menu.Item key="1">1st menu item</Menu.Item>
+    <Menu.Item key="2">2nd menu item</Menu.Item>
+    <Menu.Item key="3">3d menu item</Menu.Item>
+  </Menu>
+);
+
+ReactDOM.render(
+  <div>
+    <Dropdown.Button onClick={handleButtonClick} overlay={menu}>
+      Dropdown
+    </Dropdown.Button>
+    <Dropdown.Button
+      onClick={handleButtonClick} overlay={menu}
+      disabled
+      style={{ marginLeft: 8 }}
+    >
+      Dropdown
+    </Dropdown.Button>
+    <Dropdown overlay={menu}>
+      <Button style={{ marginLeft: 8 }}>
+        Button <Icon type="down" />
+      </Button>
+    </Dropdown>
+  </div>
+, document.getElementById('root'));
+
+// ReactDOM.render(<MyTable/>, document.getElementById('root'));
