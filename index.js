f5ec29770930ffb1dcdc81ad660198f075fd952b
WingUNO <le13413580712@163.com>
2017-04-23 11:51:44 +0800

    custom sorter

--- index.js
+++ index.js
@@ -1,74 +1,114 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Select } from 'antd';
-import jsonp from 'fetch-jsonp';
-import querystring from 'querystring';
-const Option = Select.Option;
 
-let timeout;
-let currentValue;
+import { Table } from 'antd';
 
-function fetch(value, callback) {
-  if (timeout) {
-    clearTimeout(timeout);
-    timeout = null;
+const columns = [{
+  title: 'No',
+  dataIndex: 'no'
+},{
+  title: 'Name',
+  dataIndex: 'name',
+  filters: [{
+    text: 'Joe',
+    value: 'Joe',
+  }, {
+    text: 'Jim',
+    value: 'Jim',
+  }, {
+    text: 'Submenu',
+    value: 'Submenu',
+    children: [{
+      text: 'Green',
+      value: 'Green',
+    }, {
+      text: 'Black',
+      value: 'Black',
+    }],
+  }],
+  // specify the condition of filtering result
+  // here is that finding the name started with `value`
+  onFilter: (value, record) => record.name.indexOf(value) === 0,
+  sorter: (a, b) => a.name.length - b.name.length,
+}, {
+  title: 'Age',
+  dataIndex: 'age',
+  sorter: (a, b) => a.age-b.age,
+}, {
+  title: 'Address',
+  dataIndex: 'address',
+  filters: [{
+    text: 'London',
+    value: 'London',
+  }, {
+    text: 'New York',
+    value: 'New York',
+  }],
+  filterMultiple: false,
+  onFilter: (value, record) => record.address.indexOf(value) === 0,
+  sorter: (a, b) => a.address.length - b.address.length,
+}];
+
+const data = [{
+  key: '1',
+  no: 1,
+  name: 'John Brown',
+  age: 32,
+  address: 'New York No. 1 Lake Park',
+}, {
+  key: '2',
+  no: 2,
+  name: 'Jim Green',
+  age: 42,
+  address: 'London No. 1 Lake Park',
+}, {
+  key: '3',
+  no: 3,
+  name: 'Joe Black',
+  age: 32,
+  address: 'Sidney No. 1 Lake Park',
+}, {
+  key: '4',
+  no: 4,
+  name: 'Jim Red',
+  age: 32,
+  address: 'London No. 2 Lake Park',
+}];
+
+// function onChange(pagination, filters, sorter) {
+//   console.log('params', pagination, filters, sorter);
+// }
+
+class MyTable extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state={
+      data:data,
+      columns:columns
+    }
   }
-  currentValue = value;
 
-  function fake() {
-    const str = querystring.encode({
-      code: 'utf-8',
-      q: value,
-    });
-    jsonp(`https://suggest.taobao.com/sug?${str}`)
-      .then(response => response.json())
-      .then((d) => {
-        if (currentValue === value) {
-          const result = d.result;
-          const data = [];
-          result.forEach((r) => {
-            data.push({
-              value: r[0],
-              text: r[0],
-            });
-          });
-          callback(data);
-        }
-      });
+  onChange = (pagination, filters, sorter) => {
+    console.log(pagination, filters, sorter);
+    // let data = [...this.state.data];
+    // data.sort((a, b) => {
+    //   return sorter.order == 'ascend' ? a[sorter.columnKey] - b[sorter.columnKey] : b[sorter.columnKey] - a[sorter.columnKey];
+    // })
+    // data.forEach((value, index) => {
+    //   value.no = index + 1;
+    // })
+    // this.setState({
+    //   data
+    // })
+    // this.forceUpdate();
   }
 
-  timeout = setTimeout(fake, 300);
-}
-
-class SearchInput extends React.Component {
-  state = {
-    data: [],
-    value: '',
-  }
-  handleChange = (value) => {
-    this.setState({ value });
-    fetch(value, data => this.setState({ data }));
-  }
   render() {
-    const options = this.state.data.map(d => <Option key={d.value}>{d.text}</Option>);
+    console.log('update.....');
     return (
-      <Select
-        mode="combobox"
-        value={this.state.value}
-        placeholder={this.props.placeholder}
-        notFoundContent=""
-        style={this.props.style}
-        defaultActiveFirstOption={false}
-        showArrow={false}
-        filterOption={false}
-        onChange={this.handleChange}
-      >
-        {options}
-      </Select>
+      <Table columns={this.state.columns} dataSource={this.state.data} onChange={this.onChange} />
     );
   }
 }
 
-ReactDOM.render(
-  <SearchInput placeholder="input search text" style={{ width: 200 }} />
-, document.getElementById('root'));
+ReactDOM.render(<MyTable/>, document.getElementById('root'));
