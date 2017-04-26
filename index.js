bfe1e65f9ded7e5776e788c4b3ba22c490916e01
WingUNO <le13413580712@163.com>
2017-04-26 11:16:36 +0800

    Tooltip

--- index.js
+++ index.js
@@ -1,67 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { Menu, Dropdown, Button, Icon, message } from 'antd';
-
-function handleButtonClick(e) {
-  message.info('Click on left button.');
-  console.log('click left button', e);
-}
-
-function handleMenuClick(e) {
-  message.info('Click on menu item.');
-  console.log('click', e);
-}
+import { Tooltip, Button } from 'antd';
 
 class MyComponent extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: ''
-    }
-  }
-
-  handleChange = (e) => {
-    this.setState({
-      value: e.target.value.trim()
-    });
-  }
-
   render() {
     return (
       <div>
-        <input type='text' onChange={this.handleChange} value={this.state.value}/>
+        <Button>哈哈哈</Button>
+        <Button>哈哈哈</Button>
+        <Button>哈哈哈</Button>
       </div>
     );
   }
 }
 
-const menu = (
-  <Menu onClick={handleMenuClick}>
-    <Menu.Item key="1">1st menu item</Menu.Item>
-    <Menu.Item key="2">2nd menu item</Menu.Item>
-    <Menu.Item key="3">3d menu item</Menu.Item>
-  </Menu>
-);
-
 ReactDOM.render(
   <div>
-    <Dropdown.Button onClick={handleButtonClick} overlay={menu}>
-      Dropdown
-    </Dropdown.Button>
-    <Dropdown.Button
-      onClick={handleButtonClick} overlay={menu}
-      disabled
-      style={{ marginLeft: 8 }}
-    >
-      Dropdown
-    </Dropdown.Button>
-    <Dropdown overlay={menu}>
-      <Button style={{ marginLeft: 8 }}>
-        Button <Icon type="down" />
-      </Button>
-    </Dropdown>
-    <MyComponent />
+    <Tooltip placement="topLeft" title={<MyComponent/>}>
+      <Button>Align edge / 边缘对齐</Button>
+    </Tooltip>
+    <Tooltip placement="topLeft" title="Prompt Text" arrowPointAtCenter>
+      <Button>Arrow points to center / 箭头指向中心</Button>
+    </Tooltip>
   </div>
 , document.getElementById('root'));
 
